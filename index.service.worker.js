775f76c295d0d75693d17370d99a88a16dd4c410
Philipp Steinhaus <p.steinhaus@proton.me>
2026-05-31 22:42:21 +0200

    added transparence

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780250961|16005330360';
+const CACHE_VERSION = '1780260103|23368590259';
 /** @type {string} */
 const CACHE_PREFIX = 'Mathtrees-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
