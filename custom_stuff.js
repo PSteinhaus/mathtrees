bc964b60447b61044ea73fbe3e1ce9a8c982e0d3
Philipp Steinhaus <p.steinhaus@proton.me>
2026-05-31 20:11:16 +0200

    PWA audio fix, and icon attempt 3

--- custom_stuff.js
+++ custom_stuff.js
@@ -1,11 +1,20 @@
+const OriginalAudioContext = window.AudioContext;
+
+window.AudioContext = function (...args) {
+    const ctx = new OriginalAudioContext(...args);
+    window.myAudioContext = ctx;
+    console.log("Captured AudioContext:", ctx);
+    return ctx;
+};
+
+window.AudioContext.prototype = OriginalAudioContext.prototype;
+
 document.addEventListener("visibilitychange", () => {
-    if (document.hidden) {
-        if (window.godotAudioContext) {
-            window.godotAudioContext.suspend();
-        }
-    } else {
-        if (window.godotAudioContext) {
-            window.godotAudioContext.resume();
+    if (window.myAudioContext) {
+        if (document.hidden) {
+            window.myAudioContext.suspend();
+        } else {
+            window.myAudioContext.resume();
         }
     }
 });
