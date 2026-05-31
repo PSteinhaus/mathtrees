d9064c8b67781bc62854446d2796119d6680d647
Philipp Steinhaus <p.steinhaus@proton.me>
2026-05-31 16:27:09 +0200

    new web release branch, only containing web build files

--- godot/scripts/debug_label.gd
+++
@@ -1,4 +0,0 @@
-extends Label
-
-func _process(_delta: float) -> void:
-	text = Global.debug_print_string
--- godot/scripts/global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var debug_print_string: String = ""
-var world: World
-
-func set_world(w: World):
-	world = w
-func get_world() -> World:
-	return world
