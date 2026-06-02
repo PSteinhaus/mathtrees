4eb52bbd1a676a7838e6714263fcf227f26367e0
Philipp Steinhaus <p.steinhaus@proton.me>
2026-06-02 10:53:17 +0200

    fixed shrink_factor locking in at 0.5

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780311719|4205604999';
+const CACHE_VERSION = '1780390228|278154808';
 /** @type {string} */
 const CACHE_PREFIX = 'Mathtrees-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
