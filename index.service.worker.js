ac8f4b34b836e9af0161e168f039824f0c2b1778
Philipp Steinhaus <p.steinhaus@proton.me>
2026-06-05 21:00:51 +0200

    fix culling problem on fractal tree

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780434156|16858544177';
+const CACHE_VERSION = '1780685964|4769763590';
 /** @type {string} */
 const CACHE_PREFIX = 'Fraktalgarten-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
